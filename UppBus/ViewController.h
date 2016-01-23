d8828f920a21f5c4deef08d7d0d8a82dd00272c9
Pablo García-Nieto Rodríguez <pablo@garcianieto.me>
2016-01-23 20:56:11 +0100

    Initial Commit

---
+++ UppBus/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UppBus
+//
+//  Created by Pablo García-Nieto Rodríguez  on 23/1/16.
+//  Copyright © 2016 pabgn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UppBus/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  UppBus
+//
+//  Created by Pablo García-Nieto Rodríguez  on 23/1/16.
+//  Copyright © 2016 pabgn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
